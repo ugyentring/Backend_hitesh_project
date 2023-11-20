d5de8ea4d967e321106d169276eb486e8685f5be
ugyentring <12220096.gcit@rub.edu.bt>
2023-11-20 21:59:40 +0600

    declared the register user controller and tested

--- src/db/index.js
+++ src/db/index.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
       `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
     );
   } catch (error) {
-    console.log("MONGODB connection error ", error);
+    console.log("MONGODB connection FAILED ", error);
     process.exit(1);
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-//right way to import dotenv
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 //use dotenv
 dotenv.config({
@@ -9,31 +9,10 @@ dotenv.config({
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 5000, () => {
       console.log(`Server is running at port: ${process.env.PORT}`);
     });
   })
   .catch((err) => {
     console.log("MONGODB connection failed", err);
   });
-
-/* 
-import express from "express";
-const app = express();
-
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.log("ERROR: ", error);
-      throw error;
-    });
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("ERROR: ", error);
-    throw error;
-  }
-})();
-*/
